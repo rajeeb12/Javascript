010c3df95e3e99cead0806c87468e217b519d28e
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-10-02 16:55:54 +0000

    data-type-summay

--- 0_1_basics/data-types-summary.js
+++ 0_1_basics/data-types-summary.js
@@ -11,3 +11,9 @@ const bidNumber = 489264018704704420n // bigint
 // arrays, objects - object
 
 // Js is dynamically typed language - because their is need of specifing the data-type during declaration
+
+// ***************** Stack Heap ********************
+
+// Stack (primitive) Heap(non-primitive)
+// primitive data is store in stack and while accessing it will get a copy of the data
+// non primitive data is stored in heap and its refernce(address) is begin accessed so the original data will be changed.
