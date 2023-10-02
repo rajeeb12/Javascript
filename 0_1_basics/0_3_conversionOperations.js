ec72119dcba3955ceac8e88b5c09ed88a8a09b7e
Rajeeb Bhowmick <41242706+rajeeb12@users.noreply.github.com>
2023-10-02 12:15:36 +0000

    conversion

---
+++ 0_1_basics/0_3_conversionOperations.js
@@ -0,0 +1,9 @@
+let score = 33
+let valueInNumber = Number(score) // conversion
+
+console.log(typeof(score))
+console.log(typeof(valueInNumber)) 
+
+//"33" => 33
+// "33abc" => NaN
+// true = 1 false = 0
